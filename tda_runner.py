1f41f60ad05d7cfba74d2634dbcaabf906f8df52
Alian <466007246@qq.com>
2025-08-01 23:45:34 +0800

    8.1 TDA 和 BoostAdapter 代码对比完成

--- tda_runner.py
+++ tda_runner.py
@@ -126,43 +126,77 @@ def compute_cache_logits(image_features,
     with torch.no_grad():
         # 所有缓存特征图
         cache_keys = []
-        # 将 feature 添加到特征图列表
+        # 每个缓存对应的类别或概率图
         cache_values = []
+        # 将 feature 添加到特征图列表
         for class_index in sorted(cache.keys()):
             for item in cache[class_index]:
                 cache_keys.append(item[0])
+                # 如果启用了 neg_mask_thresholds，说明当前你希望使用 item[2] 的概率图（prob_map）进行筛选或加权
                 if neg_mask_thresholds:
+                    # 缓存值中存入概率图
                     cache_values.append(item[2])
                 else:
+                    # 否则，就是普通的分类任务，将类别存入缓存
                     cache_values.append(class_index)
 
+        # 拼接所有缓存图像特征为 [N, D]，然后转置为 [D, N]
         cache_keys = torch.cat(cache_keys, dim=0).permute(1, 0)
+        # 如果启用阈值过滤
         if neg_mask_thresholds:
+            # torch.cat(..., dim=0) 把它们拼成一个 [N, D] 的 tensor
             cache_values = torch.cat(cache_values, dim=0)
+            # 阈值过滤 (val > th0) & (val < th1) 得到 [N, D] 的 mask，过滤缓存中不可靠的特征维度
+            # 随后将 bool 类型转成 int8（0 或 1），再转成 float16 并搬到 GPU
             cache_values = (((cache_values > neg_mask_thresholds[0]) &
                              (cache_values < neg_mask_thresholds[1])).type(
                                  torch.int8)).cuda().half()
         else:
+            # F.one_hot(...) → [N, C]：将每个类别索引转换成 one-hot 标签
+            # .cuda().half()：转为 float16 并搬到 CUDA
             cache_values = (F.one_hot(
                 torch.Tensor(cache_values).to(torch.int64),
                 num_classes=clip_weights.size(1))).cuda().half()
 
+        # 当前图像特征与 N 个缓存样本的特征向量相乘后得到每个缓存样本与该图像的相似度
+        # affinity.shape == [1, N]
         affinity = image_features @ cache_keys
+        # beta - beta * affinity = beta * (1 - affinity) 越相似的 affinity 趋近于 1，这项趋近于 0；越不相似的 affinity 趋近于 0，这项趋近于 beta
+        # (-1 * ...).exp() = exp(beta * (affinity - 1)) 使得越相似的 affinity 得到的权重越大（接近 1），越不相似的权重接近 0
+        # cache_logits = weight @ cache_values 是一个典型的加权平均操作
         cache_logits = ((-1) * (beta - beta * affinity)).exp() @ cache_values
+        # return alpha * cache_logits, affinity
+        # 最后输出一个加权后的向量（特征或伪 logit），乘以调节系数 alpha
+        # 返回值 shape 保持为 [1, D]
         return alpha * cache_logits
+
+
+'''
+run_test_tda(...) 用于测试 TDA 在分类任务中的准确率
+
+pos_cfg, neg_cfg：                      # 用于构建正负样本缓存的配置
+loader：                                # PyTorch 的 DataLoader 对象，加载测试集 
+clip_model：                            # CLIP 模型（或其变体），用来提取图像特征
+clip_model：                            # CLIP 模型（或其变体），用来提取图像特征
+logger：                                # 日志记录器
+args, cfg：                             # 其他控制参数或配置对象
+'''
 
 
 def run_test_tda(pos_cfg, neg_cfg, loader, clip_model, clip_weights):
     with torch.no_grad():
         pos_cache, neg_cache, accuracies = {}, {}, []
 
-        #Unpack all hyperparameters
+        # Unpack all hyperparameters
+        # 解包配置中的开关状态,从配置字典 pos_cfg, neg_cfg 中读取是否启用对应缓存机制
         pos_enabled, neg_enabled = pos_cfg['enabled'], neg_cfg['enabled']
+        # 如果启用正缓存，提取 pos_cfg 中的主要参数
         if pos_enabled:
             pos_params = {
                 k: pos_cfg[k]
                 for k in ['shot_capacity', 'alpha', 'beta']
             }
+        # 如果启用负缓存，额外会多两个筛选参数
         if neg_enabled:
             neg_params = {
                 k: neg_cfg[k]
@@ -173,29 +207,58 @@ def run_test_tda(pos_cfg, neg_cfg, loader, clip_model, clip_weights):
             }
 
         #Test-time adaptation
+        # 遍历测试集 loader，每次取出一批 images 和其真实标签 target，在遍历同时解包
+        '''
+        这是一个迭代器包裹写法
+
+        loader：                                # 通常是一个 PyTorch 的 DataLoader，会返回 (images, target) 的 batch。
+        tqdm(...)：                             # 将 loader 包裹，显示处理进度。
+        desc='Processed test images: '：        # 指定进度条前缀文本
+        '''
         for i, (images, target) in enumerate(
                 tqdm(loader, desc='Processed test images: ')):
+            # 使用目前提供的数据集时，infer_ori_image = True 总成立
+            # 获取图像特征与 CLIP 输出
+            '''
+            image_features:                 # 原图图像特征向量
+            clip_logits：                   # 原图的 zero-shot logits
+            loss：                          # 原图的 zero-shot 熵
+            prob_map：                      # 原图的 zero-shot 概率分布
+            pre：                           # 原图的 zero-shot 预测类别
+            '''
             image_features, clip_logits, loss, prob_map, pred = get_clip_logits(
                 images, clip_model, clip_weights)
+            # 将 target 移到 GPU 上，get_entropy() 可能是根据 loss 或 clip_logits 计算预测分布的不确定性（熵）
             target, prop_entropy = target.cuda(), get_entropy(
                 loss, clip_weights)
-
+            '''
+            如果启用 positive cache，就调用 update_cache() 更新缓存
+            
+            pred:                           # 当前样本原图的预测的类别；
+            [image_features, loss]:         # 要缓存的原图图像特征和其对应的 zero-shot 熵（可用于排序）；
+            shot_capacity:                  # 每类最多缓存多少个样本；
+            '''
             if pos_enabled:
                 update_cache(pos_cache, pred, [image_features, loss],
                              pos_params['shot_capacity'])
 
+            # 如果启用 negative cache 且当前样本的归一化熵（前面由 get_entropy() 得到）在给定的熵筛选区间
             if neg_enabled and neg_params['entropy_threshold'][
                     'lower'] < prop_entropy < neg_params['entropy_threshold'][
                         'upper']:
+                # 使用 update_cache 把 [image_features, loss, prob_map] 放入对应 pred 类别的 neg_cache 中
                 update_cache(neg_cache, pred, [image_features, loss, prob_map],
                              neg_params['shot_capacity'], True)
 
+            # 初始 logits 仅为 CLIP 输出，用作基础
             final_logits = clip_logits.clone()
+            # 加入正缓存得分
             if pos_enabled and pos_cache:
                 final_logits += compute_cache_logits(image_features, pos_cache,
                                                      pos_params['alpha'],
                                                      pos_params['beta'],
                                                      clip_weights)
+            # 减去负缓存得分，这样能有效抑制误分类倾向（比如视觉相似但语义不同的类别）
             if neg_enabled and neg_cache:
                 final_logits -= compute_cache_logits(
                     image_features, neg_cache, neg_params['alpha'],
@@ -203,61 +266,92 @@ def run_test_tda(pos_cfg, neg_cfg, loader, clip_model, clip_weights):
                     (neg_params['mask_threshold']['lower'],
                      neg_params['mask_threshold']['upper']))
 
+            # 使用最终融合后的 final_logits 进行分类
             acc = cls_acc(final_logits, target)
+            # 将该 batch 的准确率加入 accuracies 列表，供最后汇总
             accuracies.append(acc)
+
+            # wandb.log() 是用来上传一个或多个指标到 Weights & Biases 仪表盘的方法，一般在训练/测试过程中定期调用
+            # 在 Weights & Biases 仪表盘中，你会看到：一个名为 "Averaged test accuracy" 的图线或表格。
+            # 每次调用 wandb.log(..., commit=True)，它会成为图表中的一个点（step）
             wandb.log(
                 {"Averaged test accuracy": sum(accuracies) / len(accuracies)},
                 commit=True)
 
+            # 每隔 1000 个测试样本/批次，打印一次当前的平均测试准确率，格式化为保留两位小数的输出
             if i % 1000 == 0:
                 print("---- TDA's test accuracy: {:.2f}. ----\n".format(
                     sum(accuracies) / len(accuracies)))
+        # 打印一次最终的平均测试准确率，格式化为保留两位小数的输出
         print("---- TDA's test accuracy: {:.2f}. ----\n".format(
             sum(accuracies) / len(accuracies)))
         return sum(accuracies) / len(accuracies)
 
 
 def main():
+    # 调用 get_arguments()，解析命令行参数
     args = get_arguments()
+    # 获取配置文件路径
     config_path = args.config
 
     # Initialize CLIP model
+    # 从 args.backbone 指定路径加载 CLIP 模型和对应图像预处理方法
     clip_model, preprocess = clip.load(args.backbone)
+    # 将模型设为推理模式，关闭 dropout 等
     clip_model.eval()
 
     # Set random seed
+    # 设置随机种子（保证实验可复现）
     random.seed(1)
     torch.manual_seed(1)
 
+    # 如果你开启了 wandb 日志（通过命令行参数 --wandb），则执行以下逻辑
     if args.wandb:
+        # 当前时间
         date = datetime.now().strftime("%b%d_%H-%M-%S")
+        # wandb 分组名，用于将多个数据集下的运行结果统一归入一个组
         group_name = f"{args.backbone}_{args.datasets}_{date}"
 
     # Run TDA on each dataset
+    # 遍历多个数据集并处理，如果你设置了 --datasets imagenet/caltech101, 就会自动拆分成多个子任务
     datasets = args.datasets.split('/')
+    # 对数据集路径下每个数据集
     for dataset_name in datasets:
         print(f"Processing {dataset_name} dataset.")
 
+        # 获取设置文件
         cfg = get_config_file(config_path, dataset_name)
         print("\nRunning dataset configurations:")
         print(cfg, "\n")
 
+        # build_test_data_loader()：加载测试图像数据（可迭代的 DataLoader）、类别名称、prompt 模板
         test_loader, classnames, template = build_test_data_loader(
             dataset_name, args.data_root, preprocess)
+        # clip_classifier()：基于类别名和 prompt 模板，使用 CLIP 的 encode_text() 得到每个类别的特征向量（class centers）
         clip_weights = clip_classifier(classnames, template, clip_model)
 
+        # 启动 wandb 日志（每个数据集一个 run）
         if args.wandb:
+            '''
+            wandb.init()：              # 开始记录一个实验运行，项目名是 ETTA-CLIP。
+            config=cfg：                # 将该数据集的配置也记录到仪表盘中。
+            group=group_name：          # 每个组可能对应一次完整运行，便于多数据集对比。
+            name=run_name：             # 这个子实验的名字，就是当前数据集名
+            '''
             run_name = f"{dataset_name}"
             run = wandb.init(project="ETTA-CLIP",
                              config=cfg,
                              group=group_name,
                              name=run_name)
 
+        # 运行测试阶段的 TDA 算法，输出该数据集下的平均准确率（acc）
         acc = run_test_tda(cfg['positive'], cfg['negative'], test_loader,
                            clip_model, clip_weights)
 
         if args.wandb:
+            # 记录本次测试的准确率
             wandb.log({f"{dataset_name}": acc})
+            # 关闭当前 wandb run，释放资源
             run.finish()
 
 
